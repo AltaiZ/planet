58388d7e20955dbcfeeb1b137b973ff37b5d2a64
AltaiZ <munkh_altai@yahoo.com>
2025-12-30 15:29:26 +0800

    first

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,11 +4,7 @@ import React from "react";
 const page = () => {
   return (
     <div>
-      <div className="bg-[url(/westerlund.jpg)] h-screen w-full relative">
-        <div className="absolute bg-black/10 h-screen w-full"></div>
-        <span className="text-white text-6xl flex justify-center items-center"> Welcome to the Universe</span>
-        <span className="text-white">Discover the universe: Learn about the history of the cosmos, what it's made of, and so much more.</span>
-      </div>
+     fgawsg
     </div>
   );
 };
